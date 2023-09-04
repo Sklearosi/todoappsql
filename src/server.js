687d1ac1ac9e6eec2c7ab391f6af9c0d145c045e
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-09-05 00:22:11 +0400

    feat: modules deleted

--- src/server.js
+++ src/server.js
@@ -12,6 +12,9 @@ async function init(){
  }
 
  function serverStart(){
+    app.get("/", (req,res) => {
+        return res.status(200).json({message: "works!!!"})
+    })
     app.listen(3000)
  } 
 
